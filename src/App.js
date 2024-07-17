a3a3fb493bd251c30cf6b5bf878b50eb55cb072a
Diksha Sharma <diksha@softobiz.com>
2024-07-17 22:51:57 +0530

    intigrated with news api and components initialized

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import BlogPostDetails from "./components/BlogPostDetails";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
+import BlogPostList from "./components/BlogPostList";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        <Navbar />
+        <BlogPostList />
+      </>
+    ),
+  },
+  {
+    path: "/post/:id",
+    element: (
+      <>
+        <Navbar />
+        <BlogPostDetails />
+      </>
+    ),
+  },
+  {
+    path: "/about",
+    element: (
+      <>
+        <Navbar />
+        <About />
+      </>
+    ),
+  },
+]);
+
 function App() {
-  return (
-    <div>
-      <header>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
